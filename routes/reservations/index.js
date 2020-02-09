0b151ef67b0fc34feee2fe4bb54699a3a983d424
Jenwng <Jenniescwong@gmail.com>
2020-02-09 16:58:33 -0500

    set up get and post routes

--- routes/reservations/index.js
+++ routes/reservations/index.js
@@ -1,9 +1,16 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { Reservation } = require('../../models');
+const { Reservation } = require("../../models");
 
-router.get('/', async (req,res) => {
+router.get("/", async (req, res) => {
   res.json(await Reservation.all());
+});
+
+router.post("/new", (req, res) => {
+  console.log(req.body);
+  res.send(
+    `I received your Reservation request. This is what you sent me: ${req.body.post}`
+  );
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,13 +1,23 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
-const reservationsRouter = require('./routes/reservations');
+const port = process.env.PORT || 5000;
+const reservationsRouter = require("./routes/reservations");
+const bodyParser = require("body-parser");
+
+// // middlewares
+// app.use(express.urlencoded({ extended: true }));
+// // app.use(methodOverride("_method"));
+// app.use(express.json());
+
+// below will parse urlencoded bodies
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // serve the homepage from here
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // handle api calls from here
-app.use('/reservations', reservationsRouter);
+app.use("/reservations", reservationsRouter);
 
 app.listen(port, () => {
   console.log(`Server app listening on port ${port}!`);
