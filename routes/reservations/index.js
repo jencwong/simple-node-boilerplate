dde69e97de76fd40224a5dd28f98419c6c69ff36
Jenwng <Jenniescwong@gmail.com>
2020-02-10 06:50:36 -0500

    add a create route

--- routes/reservations/index.js
+++ routes/reservations/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { Reservation } = require("../../models");
+const { Index, Reservation } = require("./sequelize");
 
 router.get("/", async (req, res) => {
   res.json(await Reservation.all());
@@ -11,6 +12,16 @@ router.post("/new", (req, res) => {
   res.send(
     `I received your Reservation request. This is what you sent me: ${req.body.post}`
   );
+});
+
+// POST /create
+router.post("/create", (req, res) => {
+  const sql = "INSERT INTO Reservations (Name, Spot) VALUES (?, ?)";
+  const reservation = [req.body.Name, req.body.Spot];
+  db.run(sql, reservation, err => {
+    // if (err) ...
+    res.redirect("/");
+  });
 });
 
 module.exports = router;
