970a3e6a679148400660ff41f339af8d117b4f54
Jenwng <Jenniescwong@gmail.com>
2020-02-14 20:53:03 -0500

    fix the db

--- routes/reservations/index.js
+++ routes/reservations/index.js
@@ -2,27 +2,9 @@ const express = require("express");
 const router = express.Router();
 const { Reservation } = require("../../models");
 const { checkAvailability } = require("./timeslot");
-const { Reservations } = require("pg");
-const reservations = new Reservation({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true
-});
 
 router.get("/", async (req, res) => {
   res.json(await Reservation.all());
-});
-
-router.get("/db", async (req, res) => {
-  try {
-    const client = await reservations.connect();
-    const result = await client.query("SELECT * FROM test_table");
-    const results = { results: result ? result.rows : null };
-    res.render("pages/db", results);
-    client.release();
-  } catch (err) {
-    console.error(err);
-    res.send("Error " + err);
-  }
 });
 
 // GET all reservations
